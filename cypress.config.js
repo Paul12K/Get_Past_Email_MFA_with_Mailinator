2042bcd40092b47c6d445eb9a646af868f51446c
TopsProductsPaul <Paul.Kopyto@tops-products.com>
2024-06-02 15:21:15 -0400

    Added GH Secrets

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,15 @@
-const {defineConfig} = require('cypress');
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
     baseUrl: "https://dev.helper.adamstaxforms.com",
     env: {
-      email: "test1@adamstaxtestteam.testinator.com",
-      password: "Adamstaxtest123!",
-      mailinatorDomain: "adamstaxtestteam.testinator.com",
-      mailinatorInbox: "test1",
-      mailinatorApiToken: "a47b4d84f9104affafb1f5bb633ee123"
+      email: process.env.CYPRESS_EMAIL,
+      password: process.env.CYPRESS_PASSWORD,
+      mailinatorDomain: process.env.CYPRESS_MAILINATOR_DOMAIN,
+      mailinatorInbox: process.env.CYPRESS_MAILINATOR_INBOX,
+      mailinatorApiToken: process.env.CYPRESS_MAILINATOR_API_TOKEN
     },
   }
 });
